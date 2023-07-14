587b3701a1bafba92f73ea30cd0361db75bf2691
Ahmed ElGendy <elgendyahmed753@gmail.com>
2023-07-14 09:05:40 -0700

    advance

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - (01-89)
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c = 0;
+	int d = 1;
+
+	for ( ; c < 9 ; c++)
+	{
+		for (d = 1 ; d < = 9 ; d++)
+		{
+			putchar(c + 48);
+			putchar(d + 48);
+			if (c != 8 || d != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
