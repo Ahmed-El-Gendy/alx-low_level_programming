d1b89ecdda4a3866fb34bc010391f60cfadfb3bc
Ahmed ElGendy <elgendyahmed753@gmail.com>
2023-07-14 07:52:16 -0700

    0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - (0-9)
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c = 0;
+
+	for ( ; c < 10 ; c++)
+	{
+		printf(c);
+	}
+	putchar('\n');
+	return (0);
+}
