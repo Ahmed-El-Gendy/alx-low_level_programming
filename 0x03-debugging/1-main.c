a162f01e5d02f0b4fc8457d7b1cbd05e8d4c77d2
Ahmed ElGendy <elgendyahmed753@gmail.com>
2023-07-18 15:20:12 -0700

    test

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,11 +13,11 @@ int main(void)
 
         i = 0;
 
-        while (i < 10)
+        /*while (i < 10)
         {
                 putchar('0' + i);
 		i++;
-        }
+        }*/
 
         printf("Infinite loop avoided! \\o/\n");
 
