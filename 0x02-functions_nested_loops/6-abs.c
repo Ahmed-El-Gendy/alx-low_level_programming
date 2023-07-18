0dc7947120f6772913e7414615f510b18b7ed38d
Ahmed ElGendy <elgendyahmed753@gmail.com>
2023-07-18 09:33:53 -0700

    pos

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - is postive
+ * @n : int to check the case
+ * Return:0 or 1 or -1
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
