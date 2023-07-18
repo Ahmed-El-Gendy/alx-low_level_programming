7163de1e0a1ae8177ff4bcc6360ed7c9df6b08af
Ahmed ElGendy <elgendyahmed753@gmail.com>
2023-07-18 08:34:38 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	return (0);
+}
