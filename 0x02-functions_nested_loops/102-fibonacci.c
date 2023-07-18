83700cc5cbdc921a32d351a64b149099e5705066
Ahmed ElGendy <elgendyahmed753@gmail.com>
2023-07-18 14:50:36 -0700

    fib

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main
+ * Description: fib
+ * Return: 0
+ */
+int main(void)
+{
+	long n=0, m=1, x;
+	printf("%i, %i, ",n, m);
+	for(x = 1; x <= 50; x++)
+	{
+		long temp = n, tem = m;
+		n = m;
+		m = temp + tem;
+		printf("%lu", m);
+		if(x < 49)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
