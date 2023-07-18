20d8f27e2024369c593d046933dd7eb3a8714f0b
Ahmed ElGendy <elgendyahmed753@gmail.com>
2023-07-18 11:52:59 -0700

    table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * times_table - table
+ *
+ * Return: null
+ */
+
+void times_table(void)
+{
+	int hrs = 0;
+
+	while (hrs < 10)
+	{
+		int mi = 0;
+
+		while (mi < 10)
+		{
+			if (hrs * mi < 10)
+			{
+				_putchar('0' + hrs * mi);
+			}
+			else
+			{
+				_putchar('0' + (hrs * mi) / 10);
+				_putchar('0' + (hrs * mi) % 10);
+			}
+			if (mi == 9)
+				_putchar('\n');
+			else
+				_putchar(' ');
+			mi++;
+		}
+		hrs++;
+	}
+}
