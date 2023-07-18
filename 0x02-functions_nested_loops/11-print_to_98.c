70e9b85210ca6e952a3f8376adec3cc631ccf9c2
Ahmed ElGendy <elgendyahmed753@gmail.com>
2023-07-18 12:18:50 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_to_98 - add
+ *@a : the first num
+ *
+ * Return: null
+ */
+
+void print_to_98(int a)
+{
+	while (a <= 98)
+	{
+		if (a < 10)
+		{
+			_putchar('0' + a);
+		}
+		else
+		{
+			_putchar('0' + (a / 10));
+			_putchar('0' + (a % 10));
+		}
+		_putchar(',');
+		_putchar(' ');
+	}
+}
