701eaa1dad4ef96a44e404d31ee30e035b28b629
Ahmed ElGendy <elgendyahmed753@gmail.com>
2023-07-18 14:33:15 -0700

    100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,7 +7,7 @@
 
 void print_times_table(int n)
 {
-	int hrs = 0, mi;
+	int hrs = 0, mi,s;
 
 	if (n > 15 || n < 0)
 		return;
@@ -16,25 +16,26 @@ void print_times_table(int n)
 	{
 		for (mi = 0; mi <= n; mi++)
 		{
+			s = a * b;
 			if (mi == 0)
-				_putchar('0' + hrs * mi);
-			else if (hrs * mi < 10)
+				_putchar('0' + s);
+			else if (s < 10)
 			{
 				_putchar(' ');
 				_putchar(' ');
-				_putchar('0' + (hrs * mi));
+				_putchar('0' + s);
 			}
-			else if (hrs * mi < 100)
+			else if (s < 100)
 			{
 				_putchar(' ');
-				_putchar('0' + (hrs * mi) / 10);
-				_putchar('0' + (hrs * mi) % 10);
+				_putchar('0' + s / 10);
+				_putchar('0' + s % 10);
 			}
 			else
 			{
-				_putchar('0' + ((hrs * mi) / 100));
-				_putchar('0' + (((hrs * mi) - 100) / 10));
-				_putchar('0' + ((hrs * mi) % 100));
+				_putchar('0' + (s / 100));
+				_putchar('0' + (( - 100) / 10));
+				_putchar('0' + (s % 100));
 			}
 			if (mi < n)
 			{
