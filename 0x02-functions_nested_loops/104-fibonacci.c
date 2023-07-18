bd4721bcf26017680c4993c8144c0022c272717a
Ahmed ElGendy <elgendyahmed753@gmail.com>
2023-07-18 15:08:39 -0700

    finaly: git push

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,17 +6,34 @@
  */
 int main(void)
 {
-	unsigned long n = 0, m = 1, x;
+	unsigned long i;
+	unsigned long before = 1;
+	unsigned long after = 2;
+	unsigned long l = 1000000000;
+	unsigned long before1;
+	unsigned long before2;
+	unsigned long after1;
+	unsigned long after2;
 
-	for (x = 1; x <= 90; x++)
+	printf("%lu", before);
+	for (i = 1; i < 91; i++)
 	{
-		unsigned long temp = n, tem = m;
-
-		n = m;
-		m = temp + tem;
-		printf("%lu", m);
-		if (x <= 89)
-			printf(", ");
+		printf(", %lu", after);
+		after += before;
+		before = after - before;
+	}
+	before1 = (before / l);
+	before2 = (before % l);
+	after1 = (after / l);
+	after2 = (after % l);
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", after1 + (after2 / l));
+		printf("%lu", after2 % l);
+		after1 = after1 + before1;
+		before1 = after1 - before1;
+		after2 = after2 + before2;
+		before2 = after2 - before2;
 	}
 	printf("\n");
 	return (0);
