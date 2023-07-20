ea9cf0b92ac80d5c20e2aaffd604beb762178577
Ahmed ElGendy <elgendyahmed753@gmail.com>
2023-07-20 12:36:18 -0700

    Add 3-print_numbers.c file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print from 0 to 9
+ * Return: null
+ */
+int print_numbers(void)
+{
+	char c = '0';
+
+	for (c = '0'; c <= '9' ; c++)
+	{
+		_putchar(c);
+	}
+}
