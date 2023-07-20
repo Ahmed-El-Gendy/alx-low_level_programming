c5c2c5bd695e74f64de6b7043d67895f0abc3636
Ahmed ElGendy <elgendyahmed753@gmail.com>
2023-07-20 12:21:02 -0700

    Add 1-isdigit.c file

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _isupper - checks if the char is integer
+ * _isdigit - checks if the char is integer
  * @c:  is the int that will use for the argument of the function
  * Return: 1 if uppercase character 0 if not
  */
-int _isupper(int c)
+int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
 		return (1);
