6b7dfa0dd8d607b48725f18c923a60f9c57e632b
Ahmed ElGendy <elgendyahmed753@gmail.com>
2023-07-20 12:54:27 -0700

    Add 5-more_numbers.c file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * more_numbers - print from 0 to 9 without 2 and 4
+ * Return: null
+ */
+void more_numbers(void)
+{
+	int c;
+	int d;
+
+	for (d = 0; d < 10; d++)
+	{
+		bool f = false;
+
+		for (c = 0; c <= 9 ; c++)
+		{
+			if (c == 4 && f)
+			{
+				break;
+			}
+			else
+			{
+				_putchar(c + 48);
+				f = true;
+			}
+			if (c == 9)
+				c = 0;
+	}	}
+	_putchar('\n');
+/*
+ * just with 2 put_char
+ */
+}
