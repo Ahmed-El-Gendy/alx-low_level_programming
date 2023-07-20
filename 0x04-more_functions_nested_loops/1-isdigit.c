1f2ee51fb282db1cffc64c9923b37584be71d068
Ahmed ElGendy <elgendyahmed753@gmail.com>
2023-07-20 10:58:27 -0700

    0:9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _isdigit - test if the num between 0 and 9
+* @c : the value of num
+* Return: 1 or 0
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	return (0);
+}
