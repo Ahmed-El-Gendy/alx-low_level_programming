ce7d1665611b2323e5bcd94c468e1e3cd557a3a6
Ahmed ElGendy <elgendyahmed753@gmail.com>
2023-07-20 13:27:51 -0700

    Add 7-print_diagonal.c file

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print ('\') n times diagonaly
+ * @n : the numper of times to print ('\')
+ * Return: null
+ */
+void print_diagonal(int n)
+{
+	int l;
+	int z;
+
+	for (l = 0; l < n; l++)
+	{
+		for (z = 0; z < l; z++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
