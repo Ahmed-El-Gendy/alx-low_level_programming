5a81fb66a724f468f258330c21f18e903e9dbb7d
Ahmed ElGendy <elgendyahmed753@gmail.com>
2023-07-20 16:18:23 -0700

    Add 10-print_triangle.c file

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - print ('#') as triangle
+ * @n : the numper of times to print ('#')
+ * Return: null
+ */
+void print_triangle(int n)
+{
+	int l = 0;
+	int z;
+
+	while (l < n && n > 0)
+	{
+		z = 0;
+		while (z < n - l - 1)
+		{
+			_putchar(' ');
+			z++;
+		}
+		int c = l;
+
+		while (c >= 0)
+		{
+			_putchar('\\');
+			c--
+		}
+		_putchar('\n');
+		l++;
+	}
+	if (l == 0)
+		_putchar('\n');
+}
