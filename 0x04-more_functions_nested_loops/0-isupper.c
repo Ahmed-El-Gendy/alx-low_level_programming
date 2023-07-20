cd2cc2d160ffab9402e65b7b805faced737a5d8c
Ahmed ElGendy <elgendyahmed753@gmail.com>
2023-07-20 09:42:22 -0700

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - test if the char is upper
+* @c : the value of char
+* Return: 1 or 0
+*/
+
+int main(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
