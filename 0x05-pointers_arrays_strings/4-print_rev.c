61a8b84598eb50aca269c8d4e0c15980ebd9cc80
Ahmed ElGendy <elgendyahmed753@gmail.com>
2023-07-24 08:39:44 -0700

    Add 4-print_rev.c file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string, in reverse
+ * @str:  is the string that will use for the argument of the function
+ * Return: NULL
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0;; i++)
+	{
+		if (str[i] == '\0')
+		{
+			break;
+		}
+	}
+	i--;
+	for (i; i >= 0; i--)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
