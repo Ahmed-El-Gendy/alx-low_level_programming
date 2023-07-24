16077af657a158ee45fd7b8f58bc440d9e0736e7
Ahmed ElGendy <elgendyahmed753@gmail.com>
2023-07-24 09:13:24 -0700

    Add 6-puts2.c file

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,13 +9,14 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0;; i += 2)
+	for (i = 0;; i++)
 	{
 		if (str[i] == '\0')
 		{
 			break;
 		}
-		_putchar(str[i]);
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
