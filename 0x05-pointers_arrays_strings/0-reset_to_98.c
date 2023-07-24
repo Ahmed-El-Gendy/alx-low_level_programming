1cfe47dc50e72ffa618a7da4bc2f5b5b94bf71ef
Ahmed ElGendy <elgendyahmed753@gmail.com>
2023-07-24 08:16:32 -0700

    array&pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - change value of integer to 98
+ * @n:  is the int that will use for the argument of the function
+ * Return: NULL
+ */
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
