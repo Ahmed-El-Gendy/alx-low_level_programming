48fc7730d61d03d89ec5263b89316954f8a65413
Ahmed ElGendy <elgendyahmed753@gmail.com>
2023-07-24 09:27:24 -0700

    Add 7-puts_half.c file

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 - function that prints every other character of a string
+ * puts_half - a function that prints half of a string, followed by a new line
  * @str:  is the string that will use for the argument of the function
  * Return: NULL
  */
@@ -17,8 +17,8 @@ void puts_half(char *str)
 			break;
 		}
 	}
-
-	v = i / 2;
+	i--;
+	v = (i) / 2;
 
 	while (i > v)
 	{
