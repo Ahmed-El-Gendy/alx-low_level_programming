a3e647bf6b2dee0a37505fad19e0772f28fae707
Ahmed ElGendy <elgendyahmed753@gmail.com>
2023-07-27 04:40:11 -0700

    Add 3-strcmp.c file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,7 @@
  * *_strncpy - copy string
  * @s1: pointer destination
  * @s2: pointer source
- * Return: pointer source
+ * Return: value difference
 */
 
 int _strcmp(char *s1, char *s2)
@@ -14,7 +14,9 @@ int _strcmp(char *s1, char *s2)
 	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
+		{
 			return (s1[i] - s2[i]);
+		}
 	}
 	return (0);
 }
