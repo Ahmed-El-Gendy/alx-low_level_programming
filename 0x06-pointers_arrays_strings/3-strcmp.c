4581c2de690203eac540fd14eccce579f1b8a4d8
Ahmed ElGendy <elgendyahmed753@gmail.com>
2023-07-27 04:36:37 -0700

    Add 3-strcmp.c file

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copy string
+ * @s1: pointer destination
+ * @s2: pointer source
+ * Return: pointer source
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
