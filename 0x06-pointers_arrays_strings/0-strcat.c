8f98dc2c766dcb81a0a7f6d4df6261399391a1fe
Ahmed ElGendy <elgendyahmed753@gmail.com>
2023-07-27 01:21:21 -0700

    start

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strcat - a function that prints half of a string, followed by a new line
+ * @src:  is the string that will use for the argument of the function
+ * @dest: is the string that will use for the argument of the function
+ * Return: NULL
+ */
+char *_strcat(char *dest, char *src)
+{
+	char [
+}
