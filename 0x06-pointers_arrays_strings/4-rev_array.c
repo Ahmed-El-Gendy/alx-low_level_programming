029b121c8d6dc98510721c176460553ba41eb273
Ahmed ElGendy <elgendyahmed753@gmail.com>
2023-07-27 04:58:52 -0700

    Add 4-rev_array.c file

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,12 +10,11 @@
 void reverse_array(int *a, int n)
 {
 	int i;
-	int x;
 
 	for (i = 0; i < n / 2; i++)
 	{
-		x = a[i];
-		a[i] = a[n - i - 1];
-		a[n - i - 1] = x;
+		a[i] += a[n - i - 1];
+		a[n - i - 1] = a[i] - a[n - i - 1];
+		a[i] = a[i] - a[n - i - 1];
 	}
 }
