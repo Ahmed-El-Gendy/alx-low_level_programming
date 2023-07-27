e7a102a92e2a61e5eb5fc02f96a574454b267a85
Ahmed ElGendy <elgendyahmed753@gmail.com>
2023-07-27 01:38:40 -0700

    Add 1-strncat.c file

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - a function that prints half of a string, followed by a new line
+ * @src:  is the string that will use for the argument of the function
+ * @dest: is the string that will use for the argument of the function
+ * n: used integer
+ * Return: String
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j = 0;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	for (; src[j] != '\0' && j < n; i++, j++)
+	{
+		dest[i] = src[j];
+	}
+	return (dest);
+}
