fba5d04b8da46d0ce4bd58ab60ae5c78a0405bc1
Ahmed ElGendy <elgendyahmed753@gmail.com>
2023-08-14 08:33:17 -0700

    Add 4-new_dog.c file

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,48 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * new_dog - function that creates a new dog
+ * @name: string
+ * @age: float
+ * @owner: string
+ * Return: NULL if fail
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i = 0, j = 0, l;
+	dot_t *dogg;
+
+	while (name[i] != "\0")
+		i++;
+	while (owner[j] != "\0")
+		j++;
+	if (dogg == NULL)
+	{
+		free(dogg);
+		return (NULL);
+	}
+	dogg->name = malloc(i * sizeof(dogg->name));
+	if (dogg->name == NULL)
+	{
+		free(dogg->name);
+		free(dogg);
+		return (NULL);
+	}
+	for (l = 0; l < i; l++)
+		dogg->name[k] = name[k];
+	dogg->age = age;
+	dogg->owner = malloc(i * sizeof(dogg->owner));
+	if (dogg->owner == NULL)
+	{
+		free(dogg->owner);
+		free(dogg->name);
+		free(dogg);
+		return (NULL);
+	}
+	for (l = 0; l < j; l++)
+		dogg->owner[k] = owner[k];
+	return (dogg);
+
+}
