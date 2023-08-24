b8d7a2ea96b71b38cfa5ba1e20bf9b3555214b10
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-08-24 09:30:42 -0700

    100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-void bf_main(void) __attribute__ ((constructor));
+void first(void) __attribute__ ((constructor));
 /**
- * bf_main - use this function before main
+ * first - use this function before main
  */
-void bf_main(void)
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n
 			I bore my house upon my back!\n");
