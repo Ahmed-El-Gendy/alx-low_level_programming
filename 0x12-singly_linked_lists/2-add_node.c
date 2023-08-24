8bffca64a6c35f1468422a04b7d8082171d148a3
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-08-24 06:56:06 -0700

    2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - add node to the end of list
+ * @head: pointer to head
+ * Return: size
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t Node* node = malloc(sizeof(list_t));
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+	if (!node)
+		return (NULL);
+	node->str = strdup(str);
+	node->len = len;
+	node->next = (*head);
+
+	(*head) = node;
+	return (*head);
+
+}
