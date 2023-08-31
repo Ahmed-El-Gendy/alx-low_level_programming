033b0b70dc6f6fc21e6e306e145fff8a745af4cf
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-08-31 14:14:59 -0700

    Add 2-get_bit.c file

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * get_bit - get the bit
+ * @n: int
+ * @index: index
+ * Return: void
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+
+	i = 1;
+	while (i < n)
+	{
+		i = (i << 1);
+	}
+	if (i < n)
+		return (-1);
+	if ((1 << index) & n)
+	{
+		return (1);
+	}
+	return (0);
+}
