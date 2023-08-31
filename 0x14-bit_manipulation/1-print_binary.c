7fcfe4012fd14a768b1db0593360490788fd10e5
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-08-31 13:50:52 -0700

    Add 1-print_binary.c file

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_binary - convert from decimal to binary
+ * @n: int
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int i;
+
+	i = 1;
+	while (i <= n)
+		(i = i << 1);
+	if (i == 1)
+	{
+		_putchar('0' + (i & n));
+		return;
+	}
+	i = (i >> 1);
+	while (i > 0)
+	{
+		if (i & n)
+		{
+			_putchar('1');
+		}
+		else
+			_putchar('0');
+		i = (i >> 1);
+	}
+
+}
