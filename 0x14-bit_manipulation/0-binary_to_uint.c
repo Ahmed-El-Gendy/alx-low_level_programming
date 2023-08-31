33578ec073b4323ce0a0cbb55a3c4d7d57355ef3
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-08-31 13:26:10 -0700

    Add 0-binary_to_uint.c file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,21 +7,24 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int sum, pow;
+	int size;
+
 	if (b == NULL)
 		return (0);
-	unsigned int i, sum = 0, size = 0, pow = 1;
+	sum = 0;
+	pow = 1;
+	size = 0;
 
-	char *a = b;
-
-	while (a[size] != '\0')
+	while (b[size] != '\0')
 		size++;
-	for (i = size - 1; i >= 0; i--)
+	for (size = size - 1; size >= 0; size--)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[size] != '0' && b[size] != '1')
 		{
 			return (0);
 		}
-		if (b[i] == '1')
+		if (b[size] == '1')
 		{
 			sum += pow;
 		}
