2e9e2837fc607e8dd0e1987a809752f39c4f69b4
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-09-04 13:46:13 -0700

    Add 0-read_textfile.c file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,34 +7,28 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	char *buf;
+	int fd;
+	ssize_t re;
+	ssize_t wr;
+
 	if (filename == NULL)
 		return (0);
-	int fd;
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-	char *buf = malloc(letters);
+	buf = malloc(sizeof(char) * letters);
 
 	if (buf == NULL)
 	{
-		close(fd);
 		return (0);
 	}
-	ssize_t re = read(fd, buf, letters);
-
-	if (re == -1)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 	{
-		close(fd);
 		free(buf);
 		return (0);
 	}
-	ssize_t wr = write(STDOUT_FILENO, buf, re);
-
+	re = read(fd, buf, letters);
+	wr = write(STDOUT_FILENO, buf, re);
 	close(fd);
-	free(buf);
-	if (re == wr)
-		return (re);
-	else
-		return (0);
+	return (wr);
 }
