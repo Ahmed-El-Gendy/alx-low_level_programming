8458480d5f22f2d17876990b9849f2a115ef634f
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-09-04 14:05:47 -0700

    ficx

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,41 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-
 /**
- * read_textfile - that reads a text file and prints
- * @filename: variable pointer
- * @letters: size letters
- * Description: Write a function that reads a text file and prints it
- * to the POSIX standard output.
- * Return: the actual number of letters it could read and print, 0 otherwise
+ * read_textfile - function that reads a text file
+ * @filename: pointer to the file
+ * @letters: the numper of letters
+ * Return: number of letters
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t file, let, w;
-	char *text;
+	char *buf;
+	ssize_t fd, re, wr;
 
-	text = malloc(letters);
-	if (text == NULL)
+	buf = malloc(letters);
+	if (buf == NULL)
 		return (0);
-
 	if (filename == NULL)
 		return (0);
-
-	file = open(filename, O_RDONLY);
-
-	if (file == -1)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 	{
-		free(text);
+		free(buf);
 		return (0);
 	}
-
-	let = read(file, text, letters);
-
-	w = write(STDOUT_FILENO, text, let);
-
-	close(file);
-
-	return (w);
+	re = read(fd, buf, letters);
+	if (re == -1)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
+	wr = write(STDOUT_FILENO, buf, re);
+	if (wr == -1)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
+	close(fd);
+	if (we == re)
+	{
+		return (wr);
+	}
+	else
+		return (0);
 }
