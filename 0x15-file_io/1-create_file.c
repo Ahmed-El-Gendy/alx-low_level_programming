1acd44c06fa79ee2b27a79774f63e68dc41ff95f
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-09-04 14:24:41 -0700

    s

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+/**
+ * _strlen - compute the length of string
+ * @str: the string
+ * Return: the length of str
+ */
+ssize_t _strlen(const char *str)
+{
+	ssize_t len = 0;
+
+	if (!str)
+		return (-1);
+
+	while (*str++)
+		len++;
+
+	return (len);
+}
+
+/**
+ * create_file - create a file
+ * @filename: name of the file
+ * @text_content: the data to write
+ * Return: int
+ */
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t b_written;
+	ssize_t fd;
+
+	b_written = 0;
+
+	if (!filename)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (fd == -1)
+		
+	{
+		return (-1);
+	}
+
+	if (text_content)
+	{
+		b_written = write(fd, text_content, _strlen(text_content));
+	}
+
+	close(fd);
+
+	if (b_written == -1 0)
+		return (-1);
+	else
+		return (1);
+}
