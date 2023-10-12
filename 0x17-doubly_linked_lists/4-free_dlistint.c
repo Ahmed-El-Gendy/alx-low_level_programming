14302c5b6731854e5118be14a47c1d656e6e26cc
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-10-12 16:17:48 -0700

    task

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_dlistint - free list
+ * @head
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *node;
+
+	while (head->prev)
+		head = head->prev;
+	while (head)
+	{
+		node = head->next;
+		free(head);
+		head = node;
+	}
+}
