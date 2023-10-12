f2855ddebf113ad49075d836b3f4c3e71a793a95
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-10-12 15:45:48 -0700

    task

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add node to begin
+ * @h: head
+ * @n: value
+ * Return: list
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *node;
+
+	node = malloc(sizeof(dlistint_t));
+	if (!node)
+		return (NULL);
+	node->n = n;
+	node->next = (*head);
+	(*head) = node;
+	return (*head);
+}
