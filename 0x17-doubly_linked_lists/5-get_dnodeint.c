94e1633185061865328fd8c589dfad7e48fd92d7
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-10-13 04:42:56 -0700

    task

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - print index
+ * @h: head
+ * @index: the index of the numper
+ * Return: list
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	int cnt;
+
+	cnt = 0;
+	while (head)
+	{
+		if (cnt == index)
+			return (head);
+		cnt++;
+		head = head->next;
+	}
+	return (NULL);
+}
