9f6f6a8eb2f48e52dde935d2a5163d52c101ad60
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-10-13 04:54:31 -0700

    task

---
+++ 0x17-doubly_linked_lists/res.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+largest_palindrome = 0
+
+for num1 in range(100, 1000):
+    for num2 in range(100, 1000):
+        product = num1 * num2
+        if str(product) == str(product)[::-1] and product > largest_palindrome:
+            largest_palindrome = product
+
+with open("102-result", "w") as file:
+    file.write(str(largest_palindrome))
