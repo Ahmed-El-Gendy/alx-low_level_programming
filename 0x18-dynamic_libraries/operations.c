50f9dd4a7f86a43975ee214b1d3a00fa27d25f80
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-10-16 08:03:07 -0700

    task

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,52 @@
+int add(int i, int k);
+int sub(int i, int k);
+int mul(int i, int k);
+int mod(int i, int k);
+int div(int i, int k);
+
+/**
+* add - adds
+* @i: integer
+* @k: integer
+* Return: sum
+*/
+int add(int i, int k)
+	return (i + k);
+
+/**
+* sub - subtracts
+* @i: integer
+* @k: integer
+* Return: the subtract
+*/
+int sub(int i, int k)
+	return (i - k);
+
+/**
+* mul - multiplies
+* @i: integer
+* @k: integer
+* Return: multiple
+*/
+int mul(int i, int k)
+	return (i * k);
+
+/**
+* div - divide
+* @i: integer
+* @k: integer
+* Return: divide
+*/
+int div(int i, int k)
+	return (i / k);
+
+/**
+* mod -modulus
+* @i: integer
+* @k: integer
+* Return: modulus
+*/
+int mod(int i, int k)
+{
+	return (i % k);
+}
