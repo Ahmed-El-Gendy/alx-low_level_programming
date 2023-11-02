f4b0f91fbface7a233fc46f5f60a8928c1895c87
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-11-02 04:30:05 -0700

    task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	p->size = size;
-	p->array = malloc(sizeof(hash_table_t *) * size);
+	p->array = malloc(sizeof(hash_node_t *) * size);
 	if (p->array == NULL)
 	{
 		free(p);
