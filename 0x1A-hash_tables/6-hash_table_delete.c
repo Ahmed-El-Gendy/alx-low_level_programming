2f4e83edddb7ed293bc477b44914167173dbe896
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-11-03 05:55:36 -0700

    task

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - delete hash table
+ * @ht: hash table
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int index;
+	hash_node_t *curr, *t;
+
+	if (ht)
+	{
+		for (index = 0; index < ht->size; index++)
+		{
+			t = curr;
+			curr = curr->next;
+			free(t->key);
+			free(t->value);
+			free(t);
+		}
+		free(ht->array);
+		free(ht);
+	}
+}
